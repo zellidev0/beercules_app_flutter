02c0b6f5c956b3f8c86d8e9f2b0eb50c290b1726
Julian Zellner <julian@zellner.cc>
2022-05-20 13:08:20 +0200

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,18 @@ import 'package:beercules/theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 late BeamerDelegate globalBeamerDelegate;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(await buildApp());
 }
 
