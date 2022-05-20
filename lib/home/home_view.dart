51fb1c0f62bfe91cdb0db792b9d8822f75c0081d
Julian Zellner <julian@zellner.cc>
2022-05-20 14:00:23 +0200

    added legal notice

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -4,6 +4,8 @@ import 'package:beercules/scaffold_widget.dart';
 import 'package:beercules/theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../common.dart';
@@ -16,24 +18,59 @@ class HomeView extends ConsumerWidget {
     final HomeController controller =
         ref.read(providers.homeController.notifier);
     return ScaffoldWidget(
-      child: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _buildTitle(),
-            const SizedBox(height: 8),
-            _buildSubTitle(),
-            const SizedBox(height: 32),
-            _buildLogo(controller: controller),
-            const SizedBox(height: 32),
-            _buildButtons(controller: controller),
-          ],
+      child: Stack(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.topRight,
+            child: _buildInfoIconButton(context),
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _buildTitle(),
+              const SizedBox(height: 8),
+              _buildSubTitle(),
+              const SizedBox(height: 32),
+              _buildLogo(controller: controller),
+              const SizedBox(height: 32),
+              _buildButtons(controller: controller),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ]),
     );
   }
+
+  IconButton _buildInfoIconButton(BuildContext context) => IconButton(
+        color: Theme.of(context).colorScheme.primary,
+        splashColor: Theme.of(context).colorScheme.onPrimary,
+        splashRadius: 16,
+        onPressed: () async {
+          var legalNotice = await rootBundle.loadString(
+            'assets/legal/${"general.legal_notice_path".tr()}',
+          );
+          await showModalBottomSheet(
+            builder: (BuildContext context) => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Html(
+                  data: legalNotice,
+                ),
+              ),
+            ),
+            context: context,
+          );
+        },
+        icon: const Icon(
+          Icons.info,
+        ),
+      );
 
   Widget _buildTitle() => const Text(
         'home_view.title',
