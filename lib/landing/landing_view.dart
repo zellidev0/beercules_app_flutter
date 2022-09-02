5786a3bc78e21161c90fe08d0d21a08f814dc06a
Julian Zellner <julian@zellner.cc>
2022-09-02 23:11:25 +0200

    added more landing page content

--- lib/landing/landing_view.dart
+++ lib/landing/landing_view.dart
@@ -3,6 +3,7 @@ import 'package:beercules/theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LandingView extends ConsumerWidget {
   const LandingView({Key? key}) : super(key: key);
@@ -12,81 +13,119 @@ class LandingView extends ConsumerWidget {
         useSafeAre: false,
         child: ScrollConfiguration(
           behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
-          child: SingleChildScrollView(
-            child: LayoutBuilder(
-              builder: (context, constraints) => Column(
-                children: [
-                  _buildTopContent(constraints: constraints, context: context),
-                  _buildReviews(),
-                ],
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                toolbarHeight: 80,
+                flexibleSpace: FlexibleSpaceBar(title: _buildAppBarContent()),
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                pinned: true,
+                floating: false,
+                expandedHeight: MediaQuery.of(context).size.height * 0.15,
               ),
-            ),
+              SliverList(
+                  delegate: SliverChildListDelegate([
+                LayoutBuilder(
+                  builder: (context, constraints) => Column(
+                    children: [
+                      _buildSloganAndMockup(
+                        constraints: constraints,
+                        context: context,
+                      ),
+                      _divider(),
+                      _buildFeatures(),
+                      _divider(),
+                      _buildReviews(),
+                      _buildLegal(),
+                      const SizedBox(height: 16),
+                    ],
+                  ),
+                ),
+              ]))
+            ],
           ),
         ),
       );
 
-  Widget _buildReviews() => Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 64.0, vertical: 16),
-            child: Text(
-              'landing_view.reviews.title'.tr(),
-              style: TextStyles.header4,
-            ).tr(),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                buildReviewCard(
-                  textResource: 'landing_view.reviews.review1.reviewText',
-                  authorResource: 'landing_view.reviews.review1.reviewAuthor',
-                  amountOfFullStar: 5,
-                ),
-                buildReviewCard(
-                  textResource: 'landing_view.reviews.review2.reviewText',
-                  authorResource: 'landing_view.reviews.review2.reviewAuthor',
-                  amountOfFullStar: 5,
-                ),
-                buildReviewCard(
-                  textResource: 'landing_view.reviews.review3.reviewText',
-                  authorResource: 'landing_view.reviews.review3.reviewAuthor',
-                  amountOfFullStar: 4,
-                ),
-                buildReviewCard(
-                  textResource: 'landing_view.reviews.review4.reviewText',
-                  authorResource: 'landing_view.reviews.review4.reviewAuthor',
-                  amountOfFullStar: 5,
-                ),
-              ],
+  Widget _buildLegal() => Text(
+        'general.legal_google_play_notice'.tr(),
+        style: TextStyles.body1.copyWith(color: Colors.white.withOpacity(0.4)),
+      ).tr();
+
+  Widget _divider() => const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        child: Divider(color: Colors.white70),
+      );
+
+  Widget _buildReviews() => SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            buildReviewCard(
+              textResource: 'landing_view.reviews.review1.reviewText',
+              authorResource: 'landing_view.reviews.review1.reviewAuthor',
             ),
-          )
-        ],
+            buildReviewCard(
+              textResource: 'landing_view.reviews.review2.reviewText',
+              authorResource: 'landing_view.reviews.review2.reviewAuthor',
+            ),
+            buildReviewCard(
+              textResource: 'landing_view.reviews.review3.reviewText',
+              authorResource: 'landing_view.reviews.review3.reviewAuthor',
+              halfStar: true,
+            ),
+            buildReviewCard(
+              textResource: 'landing_view.reviews.review4.reviewText',
+              authorResource: 'landing_view.reviews.review4.reviewAuthor',
+            ),
+          ],
+        ),
       );
 
   Widget _buildFeatures() => Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 64.0, vertical: 16),
+            padding: const EdgeInsets.symmetric(horizontal: 128, vertical: 32),
             child: Text(
-              'landing_view.reviews.title'.tr(),
-              style: TextStyles.header4,
+              'general.app_description'.tr(),
+              style: TextStyles.header4.copyWith(fontWeight: FontWeight.w400),
             ).tr(),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 128),
-            child: Text(
-              'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-                  .tr(),
-              style: TextStyles.body1,
-            ).tr(),
+          Row(
+            children: [
+              const Spacer(),
+              _buildBadge(
+                height: 80,
+                badge: Image.asset("assets/legal/google-play-badge.png"),
+              ),
+              _buildBadge(
+                height: 56,
+                badge: SvgPicture.asset("assets/legal/app-store-badge.svg"),
+              ),
+              const Spacer(),
+            ],
           ),
+          const SizedBox(height: 32),
         ],
       );
 
-  Widget _buildTopContent({
+  Expanded _buildBadge({
+    required double height,
+    required Widget badge,
+  }) =>
+      Expanded(
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            child: SizedBox(height: height, child: badge),
+            onTap: () {},
+          ),
+        ),
+      );
+
+  Widget _buildSloganAndMockup({
     required BoxConstraints constraints,
     required BuildContext context,
   }) =>
@@ -94,47 +133,36 @@ class LandingView extends ConsumerWidget {
         constraints: constraints.copyWith(
           maxHeight: MediaQuery.of(context).size.height,
         ),
-        child: Column(
-          children: [
-            _buildAppBar(),
-            _buildSloganAndMockup(),
-          ],
-        ),
-      );
-
-  Widget _buildSloganAndMockup() => Expanded(
         child: Padding(
-          padding: const EdgeInsets.all(32.0),
+          padding: const EdgeInsets.all(44.0),
           child: Row(
             children: [
               Text(
                 'landing_view.best_app_slogan'.tr(),
                 style: TextStyles.header1,
               ).tr(),
-              Image.asset("assets/mockups/iphone_home_2.png"),
+              Image.asset("assets/mockups/mockup2.png"),
             ].map((_) => Expanded(child: _)).toList(),
           ),
         ),
       );
 
-  Widget _buildAppBar() => Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 64,
-              child: Image.asset("assets/images/logo.png"),
-            ),
-            Text('general.app_name'.tr(), style: TextStyles.header1).tr(),
-          ],
-        ),
+  Widget _buildAppBarContent() => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 64,
+            child: Image.asset("assets/images/logo.png"),
+          ),
+          Text('general.app_name'.tr(), style: TextStyles.header1).tr(),
+        ],
       );
 
   Widget buildReviewCard({
     required String textResource,
     required String authorResource,
-    required int amountOfFullStar,
+    bool halfStar = false,
   }) =>
       Padding(
         padding: const EdgeInsets.all(32.0),
@@ -143,7 +171,7 @@ class LandingView extends ConsumerWidget {
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           color: Colors.blue,
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -154,7 +182,7 @@ class LandingView extends ConsumerWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       ...List.generate(
-                        amountOfFullStar - 1,
+                        halfStar ? 4 : 5,
                         (index) => const Icon(Icons.star, color: Colors.white),
                       )
                           .map(
@@ -164,11 +192,8 @@ class LandingView extends ConsumerWidget {
                             ),
                           )
                           .toList(),
-                      ...[
-                        amountOfFullStar < 5
-                            ? const Icon(Icons.star_half, color: Colors.white)
-                            : const Icon(Icons.star, color: Colors.white),
-                      ]
+                      if (halfStar)
+                        const Icon(Icons.star_half, color: Colors.white)
                     ],
                   ),
                 ),
