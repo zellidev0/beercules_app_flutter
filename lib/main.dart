74689bebfa65e3d0598a2d989abf2abd9bd3825b
Julian Zellner <julian@zellner.cc>
2022-09-03 00:00:59 +0200

    added langing page stuff

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:beamer/beamer.dart';
 import 'package:beercules/customize/customize_view.dart';
 import 'package:beercules/game/game_view.dart';
 import 'package:beercules/home/home_view.dart';
+import 'package:beercules/landing/landing_view.dart';
 import 'package:beercules/navigation_service.dart';
 import 'package:beercules/rules/rules_view.dart';
 import 'package:beercules/theme.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -36,7 +38,27 @@ Future<Widget> buildApp() async {
 }
 
 BeamerDelegate createBeamerDelegate({required Reader read}) => BeamerDelegate(
-      initialPath: NavigationService.homeRouteUri,
+      initialPath: NavigationService.landingRouteUri,
+      guards: [
+        BeamGuard(
+          pathPatterns: [],
+          // perform the check on all patterns that **don't** have a match in pathPatterns
+          guardNonMatching: true,
+          // return false to redirect
+          check: (context, location) => !kIsWeb,
+          // where to redirect on a false check
+          beamToNamed: (origin, target) => NavigationService.landingRouteUri,
+        ),
+        BeamGuard(
+          pathPatterns: [NavigationService.baseUri],
+          // perform the check on all patterns that **don't** have a match in pathPatterns
+          guardNonMatching: false,
+          // return false to redirect
+          check: (context, location) => false,
+          // where to redirect on a false check
+          beamToNamed: (origin, target) => NavigationService.landingRouteUri,
+        ),
+      ],
       locationBuilder: RoutesLocationBuilder(
         routes: {
           NavigationService.homeRouteUri: (_, __, ___) => const BeamPage(
@@ -58,6 +80,11 @@ BeamerDelegate createBeamerDelegate({required Reader read}) => BeamerDelegate(
                 key: ValueKey<String>(NavigationService.customizeRouteUri),
                 title: 'Customize',
                 child: CustomizeView(),
+              ),
+          NavigationService.landingRouteUri: (_, __, ___) => const BeamPage(
+                key: ValueKey<String>(NavigationService.landingRouteUri),
+                title: 'Landing',
+                child: LandingView(),
               ),
         },
       ),
