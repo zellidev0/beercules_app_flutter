cf8ee6924eb76fce6848b98ddc64eea7372d152e
Julian <julian@zellner.cc>
2023-11-24 19:39:24 +0100

    ref: cleanup

--- lib/common/widgets/playing_card_container.dart
+++ lib/common/widgets/playing_card_container.dart
@@ -1,15 +1,19 @@
+import 'package:beercules/common/constants.dart';
 import 'package:beercules/theme.dart';
 import 'package:flutter/material.dart';
 
 class PlayingCardContainer extends StatelessWidget {
   final Widget _child;
   final VoidCallback? _onTap;
+  final EdgeInsets _padding;
   const PlayingCardContainer({
     required final Widget child,
     final void Function()? onTap,
+    final EdgeInsets padding = Constants.pagePadding,
     super.key,
   })  : _onTap = onTap,
-        _child = child;
+        _child = child,
+        _padding = padding;
 
   @override
   Widget build(final BuildContext context) => Material(
@@ -27,7 +31,7 @@ class PlayingCardContainer extends StatelessWidget {
             child: AspectRatio(
               aspectRatio: 2.5 / 3.5,
               child: Padding(
-                padding: const EdgeInsets.all(32),
+                padding: _padding,
                 child: _child,
               ),
             ),
--- lib/customize/widgets/customize_card.dart
+++ lib/customize/widgets/customize_card.dart
@@ -1,3 +1,4 @@
+import 'package:beercules/common/widgets/playing_card_container.dart';
 import 'package:beercules/theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
@@ -15,41 +16,32 @@ class CustomizeCard extends StatelessWidget {
         _onTap = onTap;
 
   @override
-  Widget build(final BuildContext context) => Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        color: BeerculesColors.primary,
-        child: InkWell(
-          borderRadius: BorderRadius.circular(12),
-          onTap: _onTap,
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Expanded(
-                  flex: 3,
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: SvgPicture.asset(
-                      'assets/instructions/${_cardKey}_pic.svg',
-                    ),
-                  ),
+  Widget build(final BuildContext context) => PlayingCardContainer(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: SvgPicture.asset(
+                  'assets/instructions/${_cardKey}_pic.svg',
                 ),
-                const SizedBox(height: 16),
-                Expanded(
-                  child: Text(
-                    'game_view.instructions.$_cardKey.title',
-                    style: TextStyles.body5,
-                    textAlign: TextAlign.center,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                  ).tr(),
-                ),
-              ],
+              ),
             ),
-          ),
+            const SizedBox(height: 16),
+            Expanded(
+              flex: 2,
+              child: Text(
+                'game_view.instructions.$_cardKey.title',
+                style: TextStyles.body5,
+                textAlign: TextAlign.center,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ).tr(),
+            ),
+          ],
         ),
       );
 }
