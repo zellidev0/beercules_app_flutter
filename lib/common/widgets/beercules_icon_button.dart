f20290df819749fa2e670e76ae09cb5f8f9db620
Julian <julian@zellner.cc>
2024-03-30 15:15:39 +0100

    ref: icon button

--- lib/common/widgets/beercules_icon_button.dart
+++ lib/common/widgets/beercules_icon_button.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-class BeerculesIconButton extends GestureDetector {
-  BeerculesIconButton({
-    required final IconData icon,
-    required final VoidCallback onPressed,
+class BeerculesIconButton extends StatelessWidget {
+  const BeerculesIconButton({
+    required this.icon,
+    required this.onPressed,
     super.key,
-  }) : super(
-          onTap: onPressed,
-          child: Icon(icon, color: Colors.white),
-        );
+  });
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(final BuildContext context) => IconButton(
+        onPressed: onPressed,
+        icon: Icon(
+          icon,
+          color: Colors.white,
+        ),
+      );
 }
