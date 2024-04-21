cbfbe107a341b0d44268d7f39457cd7122cbaf49
Julian <julian@zellner.cc>
2024-04-21 12:28:18 +0200

    chore: mocks

---
+++ test/mocks.dart
@@ -0,0 +1,9 @@
+import 'package:beercules/ui/screens/home/home_view.dart';
+import 'package:beercules/ui/screens/home/services/home_navigation_service.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks(<Type>[
+  HomeController,
+  HomeNavigationService,
+])
+class GeneratedMocks {}
