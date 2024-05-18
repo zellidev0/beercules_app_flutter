43c5a7cc16af1f5bafb1ecbd085b3f1fdbed0ae3
Julian <julian@zellner.cc>
2024-05-19 01:12:24 +0200

    ref: move card aspect ratio to constants

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -4,4 +4,6 @@ class Constants {
   static const EdgeInsets pagePadding = EdgeInsets.all(32);
   static const BorderRadius cardBorderRadius =
       BorderRadius.all(Radius.circular(12));
+
+  static const double cardAspectRatio = 2.5 / 3.5;
 }
